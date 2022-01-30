73f25d99d59ddb02ab7a471c828c9c9315911648
KrisztianEgri <krisztian.egri@goodgrind.tech>
2022-01-30 21:33:09 +0100

    URL and resource_id bug fixed, admin page scrollbar bug fixed, fs renamed to fileSystem, unused (commented) parts deleted and reformatted the code a bit

--- controller/controller.js
+++ controller/controller.js
@@ -1,33 +1,31 @@
 var Promise = require('promise');
 var axios = require('axios');
 const config = './config.json';
-const fs = require('fs');
-
+const fileSystem = require('fs');
+const median = require('median');
 
 const dataGet = () => {
   return new Promise(function (resolve, reject) {
     let mapSettings = null;
-    try{
-      if(fs.existsSync(config)) {
-        mapSettings = JSON.parse(fs.readFileSync(config)).mapSettings;
-        if(mapSettings.resourceID === null) {
+    try {
+      if (fileSystem.existsSync(config)) {
+        mapSettings = JSON.parse(fileSystem.readFileSync(config)).mapSettings;
+        if (mapSettings.resourceID === null) {
           reject(null)
         }
       } else {
         reject(null);
       }
-    } catch(err) {
-        reject(err);
+    } catch (err) {
+      reject(err);
     }
-    var data = {
-        resource_id: mapSettings.resourceID, 
-        limit: mapSettings.limit, 
-        q: mapSettings.query
-      };
-    axios.get(mapSettings.url, {
-        data: data,
-        dataType: 'jsonp'
-      })
+    let data = {
+      resource_id: mapSettings.resourceID,
+      limit: mapSettings.limit,
+      q: mapSettings.query
+    };
+    let params = "?" + (data.resource_id ? "resource_id=" + data.resource_id : "") + (data.limit ? "&limit=" + data.limit : "") + (data.q ? "&q=" + data.q : "");
+    axios.get(mapSettings.url + params)
       .then(function (response) {
         var records = response.data.result.records;
         var points = new Array();
@@ -35,21 +33,31 @@ const dataGet = () => {
         var longitudes = new Array();
         records.forEach(element => {
           let desc = "'";
-          if(element.Latitude!=0 && element.Longitude!=0) {
+          if ((element.Latitude != 0 || element.LATITUDE != 0) && (element.Longitude != 0 || element.LONGITUDE != 0)) {
             (mapSettings.selectedFields).map((field) => {
               desc += `<p>` + field + `: ` + element[field] + `</p>`
             })
             desc += "'";
-            points.push({
-              coordinate: element.Latitude + ',' + element.Longitude,
-              description: desc
-            });
-            latitudes.push(element.Latitude);
-            longitudes.push(element.Longitude);
+            if (element.Latitude) {
+              points.push({
+                coordinate: element.Latitude + ',' + element.Longitude,
+                description: desc
+              });
+              latitudes.push(element.Latitude);
+              longitudes.push(element.Longitude);
+            } else {
+              points.push({
+                coordinate: element.LATITUDE + ',' + element.LONGITUDE,
+                description: desc
+              });
+              latitudes.push(element.LATITUDE);
+              longitudes.push(element.LONGITUDE);
+            }
+
           }
         });
-        const latMed = median(latitudes);
-        const lonMed = median(longitudes);
+        let latMed = (isNaN(median(latitudes)) ? latitudes[0] : median(latitudes));
+        let lonMed = (isNaN(median(longitudes)) ? longitudes[0] : median(longitudes));
         const result = {
           "latMed": latMed,
           "lonMed": lonMed,
@@ -61,38 +69,30 @@ const dataGet = () => {
         reject(error);
       });
   });
-} 
-
-const median = arr => {
-  const mid = Math.floor(arr.length / 2),
-    nums = [...arr].sort((a, b) => a-b);
-  return arr.length % 2 !== 0 ? nums[mid] : (nums[mid-1] + nums[mid]) /2;
 }
 
 const getAll = () => {
   return new Promise(function (resolve, reject) {
     let mapSettings = null;
-    try{
-      if(fs.existsSync(config)) {
-        mapSettings = JSON.parse(fs.readFileSync(config)).mapSettings;
-        if(mapSettings.resourceID === null) {
+    try {
+      if (fileSystem.existsSync(config)) {
+        mapSettings = JSON.parse(fileSystem.readFileSync(config)).mapSettings;
+        if (mapSettings.resourceID === null) {
           reject(null)
         }
       } else {
         reject(null);
       }
-    } catch(err) {
-        reject(err);
+    } catch (err) {
+      reject(err);
     }
     var data = {
-        resource_id: mapSettings.resourceID, 
-        limit: mapSettings.limit, 
-        q: mapSettings.query
-      };
-    axios.get(mapSettings.url, {
-        data: data,
-        dataType: 'jsonp'
-      })
+      resource_id: mapSettings.resourceID,
+      limit: mapSettings.limit,
+      q: mapSettings.query
+    };
+    let params = "?" + (data.resource_id ? "resource_id=" + data.resource_id : "") + (data.limit ? "&limit=" + data.limit : "") + (data.q ? "&q=" + data.q : "");
+    axios.get(mapSettings.url + params)
       .then(function (response) {
         resolve(response);
       })
@@ -100,6 +100,6 @@ const getAll = () => {
         reject(error);
       });
   });
-} 
+}
 
-module.exports = {dataGet, getAll}
+module.exports = { dataGet, getAll }
--- routes/admin.js
+++ routes/admin.js
@@ -1,30 +1,26 @@
 var express = require('express');
 let controller = require('../controller/controller');
 var router = express.Router();
-const fs = require('fs');
+const fileSystem = require('fs');
 const config = './config.json';
 var bcrypt = require('bcrypt');
-var ping = require('ping');
 let loginData;
 let mapSettings;
 let fields;
 var axios = require('axios');
-const https = require('https');
 
-
-
-router.get('/', function(req, res) {
-    try{
-        if(fs.existsSync(config)) {
-            if(req.session.authenticated) {
-                if(mapSettings.url == null || req.query.config == 'url') {
+router.get('/', function (req, res) {
+    try {
+        if (fileSystem.existsSync(config)) {
+            if (req.session.authenticated) {
+                if (mapSettings.url == null || req.query.config == 'url') {
                     res.render('admin/admin', {
-                    authenticated: req.session.authenticated,
-                    mapSettings,
-                    step: 0
+                        authenticated: req.session.authenticated,
+                        mapSettings,
+                        step: 0
                     });
                 } else {
-                    if(req.query.config == 'query') {
+                    if (req.query.config == 'query') {
                         res.render('admin/admin', {
                             authenticated: req.session.authenticated,
                             mapSettings,
@@ -32,21 +28,21 @@ router.get('/', function(req, res) {
                             fields: fields
                         });
                     }
-                    controller.getAll().then(function(val) {
+                    controller.getAll().then(function (val) {
                         fields = val.data.result.fields;
                         res.render('admin/admin', {
                             authenticated: req.session.authenticated,
                             mapSettings,
                             step: 1,
-                            fields: fields
+                            fields: fields,
+                            selectedFields: mapSettings.selectedFields
                         });
-                    }, function(error) {
-                            console.log(error);
-                            res.render('error', {
-                                error: {
-                                    message: error
-                                }
-                            });
+                    }, function (error) {
+                        res.render('error', {
+                            error: {
+                                message: error
+                            }
+                        });
                     });
                 }
             } else {
@@ -55,20 +51,18 @@ router.get('/', function(req, res) {
                 });
             }
         } else {
-          res.render('admin/firstrun');
+            res.render('admin/firstrun');
         }
-    } catch(err) {
+    } catch (err) {
         response.render('error', {
             error: {
                 message: err
             }
         });
     }
-    
-    
 })
 
-router.post('/firstrun', async function(req, res) {
+router.post('/firstrun', async function (req, res) {
     var cfgData = {
         loginData: {
             username: null,
@@ -92,32 +86,33 @@ router.post('/firstrun', async function(req, res) {
         username: req.body.username,
         password: encryptedPassword
     };
-    try{
-        fs.writeFileSync(config, JSON.stringify(cfgData, null, 4));
+    try {
+        fileSystem.writeFileSync(config, JSON.stringify(cfgData, null, 4));
         res.redirect('/admin');
-    } catch(err) {
+    } catch (err) {
         response.render('error', {
             error: {
                 message: err
             }
         });
     }
-    
-
-
 })
 
-router.post('/auth', async function(request, response) {
-    try{
-        const loadedConfig = JSON.parse(fs.readFileSync(config));
+router.post('/auth', async function (request, response) {
+    try {
+        const loadedConfig = JSON.parse(fileSystem.readFileSync(config));
         loginData = loadedConfig.loginData;
         mapSettings = loadedConfig.mapSettings;
-    } catch(err) {
-        response.send(err.message);
+    } catch (err) {
+        response.render('error', {
+            error: {
+                message: err
+            }
+        });
     }
-	if (loginData && loginData.username === request.body.username) {
+    if (loginData && loginData.username === request.body.username) {
         const validPassword = await bcrypt.compare(request.body.password, loginData.password);
-        if(validPassword) {
+        if (validPassword) {
             request.session.authenticated = true;
             response.redirect('/admin');
         } else {
@@ -126,91 +121,87 @@ router.post('/auth', async function(request, response) {
                     message: "Incorrect password!"
                 }
             });
-        }			
-	} else {
-		response.render('error', {
+        }
+    } else {
+        response.render('error', {
             error: {
                 message: "Incorrect username!"
             }
         });
-	}
+    }
 });
 
-router.get('/logout', function(req, res) {
+router.get('/logout', function (req, res) {
     req.session.authenticated = false;
     res.redirect('/map');
 })
 
 router.post('/save', function (req, res) {
-    switch(req.body.step) {
+    switch (req.body.step) {
         case "0":
-            const url = req.body.url
-            // const hostname = new URL(url).hostname;
-            // let isPingOK = await (await ping.promise.probe(hostname)).alive;
-
-            // if(isPingOK) {
-                mapSettings.url = url;
-                mapSettings.resourceID = req.body.resourceID;
-                try {
-                    let cfgData = {
-                        loginData,
-                        mapSettings
-                    }
-                    fs.writeFileSync(config, JSON.stringify(cfgData, null, 4));
-                    res.redirect('/admin');
-
-                } catch(err) {
-                    res.render('error', {
-                        error: {
-                            message: "Ping test OK, but an error occured during saving the config file. Please try again. Error message: " + err
-                        }
-                    });
-                }
-            // } else {
-            //     res.render('error', {
-            //         error: {
-            //             message: "Ping test error because URL may not be valid. Data not saved."
-            //         }
-            //     });
-            // } 
-            break;
-        case "1":
-            mapSettings.selectedFields = req.body.fields
-            try{
+            mapSettings.url = req.body.url;
+            mapSettings.resourceID = req.body.resourceID;
+            try {
                 let cfgData = {
                     loginData,
                     mapSettings
                 }
-                fs.writeFileSync(config, JSON.stringify(cfgData, null, 4));
+                fileSystem.writeFileSync(config, JSON.stringify(cfgData, null, 4));
+                res.redirect('/admin');
+
+            } catch (err) {
+                res.render('error', {
+                    error: {
+                        message: err
+                    }
+                });
+            }
+            break;
+        case "1":
+            mapSettings.selectedFields = req.body.fields
+            try {
+                let cfgData = {
+                    loginData,
+                    mapSettings
+                }
+                fileSystem.writeFileSync(config, JSON.stringify(cfgData, null, 4));
                 res.redirect('/');
-            } catch(err) {
+            } catch (err) {
                 res.render('error', {
                     error: {
                         message: "An error occured during saving the config file. Please try again. Error message: " + err
                     }
                 });
             }
-
-
+        case "2":
+            mapSettings.query = (req.body.query === undefined ? "" : req.body.query);
+            mapSettings.limit = (req.body.limit === undefined ? "" : req.body.limit);
+            try {
+                let cfgData = {
+                    loginData,
+                    mapSettings
+                }
+                fileSystem.writeFileSync(config, JSON.stringify(cfgData, null, 4));
+                res.redirect('/admin');
+            } catch (err) {
+                res.render('error', {
+                    error: {
+                        message: "An error occured during saving the config file. Please try again. Error message: " + err
+                    }
+                });
+            }
     }
 });
 
-router.get('/test', async function(req, res) {
+router.get('/test', async function (req, res) {
     var data = {
-        resource_id: "1a46095a-0c7a-4f5a-8e84-030884e130d8"
+        'resource_id': '1a46095a-0c7a-4f5a-8e84-030884e130d8'
     };
-    try{
-        const resp = await axios.get("http://smartme-data.unime.it/api/3/action/datastore_search", {
-            data: data,
-            dataType: 'jsonp'
-        });
-        console.log(resp)
-        res.send(JSON.stringify(resp.data.result.records))
-    } catch(error) {
-        console.log(error);
-        res.send(error);
-    }
+    axios.get('http://smartme-data.unime.it/api/action/datastore_search', {
+        data: data
+    }).then(function (response) {
+        res.send(response.data);
+    });
 });
-
 
 module.exports = router;
